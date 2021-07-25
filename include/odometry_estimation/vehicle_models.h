c323143a089aa20807cea9dce3a25da212fc5cd7
Hiwad Aziz <hiwad.aziz@gmx.de>
2021-07-25 17:52:42 +0000

    Implementation of differential drive forward kinematics.

--- include/odometry_estimation/vehicle_models.h
+++ include/odometry_estimation/vehicle_models.h
@@ -3,22 +3,33 @@
 
 #include <memory>
 
+constexpr double WHEEL_RADIUS = 0.03;
+constexpr double VEHICLE_TRACK = 0.2;
+
+struct VehicleState {
+  double x;
+  double y;
+  double yaw;
+};
+
 // Pure virtual interface for vehicle models
 class VehicleModel {
  public:
   static std::unique_ptr<VehicleModel> createConcreteVehicleModel(std::string model_name);
-  virtual void calculateNextState() = 0;
-}
+  virtual VehicleState calculateNextState(int rpm_left_wheel, int rpm_right_wheel,
+                                          VehicleState prev_state, double dt) = 0;
+};
 
 class DifferentialDriveModel : public VehicleModel {
  public:
-  void calculateNextState() override;
+  VehicleState calculateNextState(int rpm_left_wheel, int rpm_right_wheel, VehicleState prev_state,
+                                  double dt) override;
 
  private:
   DifferentialDriveModel();
   static std::unique_ptr<VehicleModel> create();
 
   friend VehicleModel;
-}
+};
 
 #endif  // VEHICLE_MODELS_H
--- src/vehicle_models.cpp
+++ src/vehicle_models.cpp
@@ -1,4 +1,6 @@
-#include "vehicle_models.h"
+#include "odometry_estimation/vehicle_models.h"
+
+#include <cmath>
 
 using VehicleModelPtr = std::unique_ptr<VehicleModel>;
 
@@ -7,9 +9,42 @@ std::unique_ptr<VehicleModel> VehicleModel::createConcreteVehicleModel(std::stri
   if (model_name.compare("DifferentialDrive") == 0) {
     return DifferentialDriveModel::create();
   }
+  else {
+    return DifferentialDriveModel::create();
+  }
 }
 
 std::unique_ptr<VehicleModel> DifferentialDriveModel::create()
 {
   return VehicleModelPtr{new DifferentialDriveModel};
+}
+
+DifferentialDriveModel::DifferentialDriveModel()
+{
+  // Intentionally blank
+}
+
+VehicleState DifferentialDriveModel::calculateNextState(int rpm_left_wheel, int rpm_right_wheel,
+                                                        VehicleState prev_state, double dt)
+{
+  VehicleState new_state{};
+  // calculate wheel velocities
+  double v_l = static_cast<double>(rpm_left_wheel) / 60 * 2 * WHEEL_RADIUS * M_PI;
+  double v_r = static_cast<double>(rpm_right_wheel) / 60 * 2 * WHEEL_RADIUS * M_PI;
+  // calculate radius of curve
+  double radius = VEHICLE_TRACK / 2 * (v_r + v_l) / (v_r - v_l);
+  // calculate ICC
+  double x_icc = prev_state.x - radius * std::sin(prev_state.yaw);
+  double y_icc = prev_state.y + radius * std::cos(prev_state.yaw);
+  // calculate angular velocity
+  double angular_vel = (v_r - v_l) / VEHICLE_TRACK;
+  // calculate forward kinematics
+  double dyaw = angular_vel * dt;
+  new_state.x =
+      std::cos(dyaw) * (prev_state.x - x_icc) - std::sin(dyaw) * (prev_state.y - y_icc) + x_icc;
+  new_state.y =
+      std::sin(dyaw) * (prev_state.x - x_icc) + std::cos(dyaw) * (prev_state.y - y_icc) + y_icc;
+  new_state.yaw = prev_state.yaw + dyaw;
+
+  return new_state;
 }
